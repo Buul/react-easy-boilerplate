5de4725acf52f3fb29e67ba74e30f28d01988a61
PAULO FIRMINO <paulo.firmino2@gmail.com>
2018-09-05 12:06:17 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../styles/app.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>React Easy Boilerplate</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
