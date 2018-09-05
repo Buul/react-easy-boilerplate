822fd38e616050e30b52a3a5469f30ce9dcee9d0
PAULO FIRMINO <paulo.firmino2@gmail.com>
2018-09-05 13:35:20 -0300

    suport ecma6 eslint+prettier

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # modern-react-boilerplate
-Basic boilerplate for React 16, Webpack 4, React Router 4
+Easy boilerplate for React 16, Webpack 4, React Router 4
 
 ## Features
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
-import '../styles/app.css'
+import React, { Component } from 'react';
+import '../styles/app.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>React Easy Boilerplate</h1>
-            </div>
-        )
-    }
+  handleChange = param => {
+    console.log(param);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>React Easy Boilerplate</h1>
+        <button type="button" onClick={() => this.handleChange('TEST ECMA6')}>
+          ECMA6
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
